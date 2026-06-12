a2d64f0623deca39836d47bfbf67ef8af28002ee
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-12 14:21:34 -0300

    Questao 3 criada

---
+++ ati.js/quest3.js
@@ -0,0 +1,12 @@
+/*
+O usuário deverá informar:
+● A distância a ser percorrida (em quilômetros);
+● O consumo médio do veículo (em km/l);
+● O preço do litro do combustível.
+Com base nessas informações, o algoritmo deve calcular e
+exibir:
+● A quantidade de combustível necessário para percorrer
+o trajeto;
+● O valor total a pagar com combustível para esse
+deslocamento.
+*/
