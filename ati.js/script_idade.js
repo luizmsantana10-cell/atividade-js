f46e221046a56b66ad4740afe99064cd9bed49f1
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-12 14:55:06 -0300

    pasta script criada

---
+++ ati.js/script_idade.js
@@ -0,0 +1,8 @@
+const formDados = document.querySelector('#formulario-pessoa')
+const divResultado = document.querySelector('#div-dados')
+
+//capturando o evento submit do formulario
+formDados.addEventListener('submit' , (evt)=>{
+    evt.preventDefault()
+
+})
