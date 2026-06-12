ee39abbc00f5215ffe0f090688b1cbd8281c6721
Luiz Miguel <luiz.m.santana10@aluno.senai.br>
2026-06-12 14:38:00 -0300

    questao 3 finalizada

--- ati.js/quest3.js
+++ ati.js/quest3.js
@@ -10,3 +10,16 @@ o trajeto;
 ● O valor total a pagar com combustível para esse
 deslocamento.
 */
+
+const prompt=require('prompt-sync')();
+
+let dist = Number(prompt('Informe a Distância em KM: '));
+let consu = Number(prompt('Informe o consumo km/l: '));
+let prec = Number(prompt('Informe a o preço do combustivel: '));
+ 
+let quant = dist / consu;
+
+let valor = prec *  quant;
+
+console.log("A quantidade de combustível necessário para percorrer o trajeto: " + quant + "litros");
+console.log("O valor total a pagar com combustível para esse deslocamento é: " + valor + "R$");
